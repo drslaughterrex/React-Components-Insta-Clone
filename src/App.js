c3ff7b12bb7ba2bb70dacb3821ed71fa004ab244
drslaughterrex <drslaughterrex@gmail.com>
2021-04-13 17:58:52 -0500

    MVP

--- src/App.js
+++ src/App.js
@@ -48,8 +48,8 @@ const App = () => {
 		<div className="App">
 			{/* Add SearchBar and Posts here to render them */}
 			{/* Check the implementation of each component, to see what props they require, if any! */}
-      <SearchBar />
-      <Post posts={posts} likePost = {likePost} />
+			<SearchBar />
+			<Post posts={posts} likePost={likePost} />
 		</div>
 	);
 };
